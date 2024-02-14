58044600496ee7d30cbc9cd5d3297b9c6d38ec6d
fossilfriend <egreenfest@gmail.com>
2024-02-14 13:23:51 -0500

    remove legacy table directory from src (still available in reference branch)

--- src/table/TableFilters/filters/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './fuzzyTextFilter';
-export * from './numericFilter';
-export * from './greaterThanFilter';
-export * from './includesFilter';
-export * from './globalTextFilter';
--- src/table/index.ts
+++
@@ -1,7 +0,0 @@
-export * from "./TableTypes";
-export * from "./TableUtils";
-export * from "./styles"
-export * from "./TableProperties";
-
-export const ROW_SELECTION_FIELD="selection";
-export const DOCUMENTATION_PATH="../../documentation/tables";
