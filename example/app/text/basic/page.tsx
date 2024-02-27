bd547729f27c005f6b7f5c6d694bee194d0f3fe5
fossilfriend <egreenfest@gmail.com>
2024-02-27 11:52:22 -0500

    clob text example

--- example/app/text/basic/page.tsx
+++ example/app/text/basic/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { ColoredText, AnnotatedText } from "@niagads-viz/Text"
+import { ColoredText, AnnotatedText, Clob } from "@niagads-viz/Text"
 
 const Page = () => {
     return (
@@ -9,7 +9,13 @@ const Page = () => {
             <br />
             <ul>
                 <li><ColoredText color="red" value="colored text" /></li>
-                <li><AnnotatedText value={{value: "annotated text", tooltip: "the AnnotatedText component associates a tooltip with a text block"}}/></li>
+                <li><AnnotatedText value={{ value: "annotated text", tooltip: "the AnnotatedText component associates a tooltip with a text block" }} /></li>
+                <li>
+                    <div style={{ width: "250px" }}>
+                        <p><em>Large Text Block (Clob) with Show/Hide:</em></p>
+                        <Clob value="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." maxLength={200} />
+                    </div>
+                </li>
             </ul>
         </>
     )
