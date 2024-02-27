240354099e0d51c2a134dc4f5fb914ef58951a24
fossilfriend <egreenfest@gmail.com>
2024-02-27 15:44:49 -0500

    LocusZoom story placeholder / needs proxy

---
+++ src/stories/LocusZoomPlot.stories.ts
@@ -0,0 +1,27 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { LocusZoomPlot } from '@components/LocusZoom';
+
+const meta: Meta<typeof LocusZoomPlot> = {
+  title: 'NIAGADS-VIZ/LocusZoom/LocusZoomPlot',
+  component: LocusZoomPlot,
+  parameters: {
+    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
+    layout: 'centered',
+  },
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
+  tags: ['autodocs'],
+};
+
+export default meta;
+type Story = StoryObj<typeof LocusZoomPlot>;
+
+export const Default: Story = {
+    args: {
+    population: 'ADSP',
+    variant: '19:44908684:T:C:rs429358',
+    genomeBuild: 'GRCh38',
+    serviceBaseUrl: '/service/locuszoom/'
+    }
+};
+
