e4964eea736ace5db3ba1acbf525e47c6148b85f
fossilfriend <egreenfest@gmail.com>
2024-05-01 14:40:45 -0400

    introduce modify and type mapping based on "type" field

--- src/components/Table/cell.d.ts
+++ src/components/Table/cell.d.ts
@@ -8,7 +8,6 @@ import {
 
 import { Color } from "@common/palettes"
 
-
 const BadgeIcons = {
     check: CheckIcon,
     solidCheck: CheckCircleIcon,
@@ -17,58 +16,59 @@ const BadgeIcons = {
     user: UserCircleIcon
 }
 
+
 export type BadgeIconType = keyof typeof BadgeIcons;
 
 export type NAString = 'n/a' | 'N/A' | 'NA' | 'na' | 'NULL' | 'null' | '.' | ''
 type RawValueType = string | number | boolean
 
+// from https://stackoverflow.com/a/55032655 -- if this works, pull out to reference
+type Modify<T, R> = Omit<T, keyof R> & R;
+
 export interface DefaultCell {
+    type: "default"
     value: RawValueType
     naString?: NAString
 }
 
-export interface FloatCell extends Omit<DefaultCell, 'value'> {
-    value: number
-    precision?: number
+export type FloatCell = Modify<DefaultCell, 
+    {type:"float", value: number, precision?: number}> 
+
+export type ScientificNotationCell = Modify<FloatCell, 
+    {type: "scientific_notation"}>
+
+export type TextCell = Modify<DefaultCell, 
+    {type: "text", truncateTo?: number}>
+
+export type AnnotatedTextCell = Modify<TextCell, 
+    { type: "annotated_text", color?: Color, tooltip?: ReactNode | string}>
+
+export type BadgeCell = Modify<AnnotatedTextCell, 
+    {type: "badge", backgroundColor?: Color, borderColor?: Color, icon?: BadgeIconType}>
+
+export type BooleanCell = Modify<BadgeCell, 
+    {
+        type:"boolean",
+        value: boolean
+        trueStr: string // what value to display for TRUE (e.g., TRUE, Yes, Y, Coding); FALSE inferred
+        nullAsFalse?: boolean // assume null === FALSE
+        falseStr?: string // if missing FALSE is displayed as empty string
+    }>
+
+export type LinkCell = Modify<AnnotatedTextCell, 
+    { type: "link", url: string, openNewTab?: boolean }>
+
+export type PercentageBarCell = Modify<FloatCell, 
+    { type: "percentage_bar", colors?: [Color, Color]}>
+
+export type NumericCells = DefaultCell | PercentageBarCell | FloatCell | ScientificNotationCell | BadgeCell
+export type StringCells = DefaultCell | AnnotatedTextCell | TextCell | BadgeCell | BooleanCell | LinkCell
+
+// https://www.typescriptlang.org/docs/handbook/2/mapped-types.html
+// https://www.typescriptlang.org/play/#code/C4TwDgpgBAogbhAdsAwge0QMwJYHMA88SwAzlBAB7BIAmZA3lANbaI0BcUJwATq7lAC+APigBeKPQBQUWVADaMKK1gJkZAIZkY8gEQs2ugLpHOACghrgnGAEpxouGmw0A3FMFSpoSFADKAI4Arho8EETI4pLMrBxQuiTBoRC6ADRQFJyIQQC2AEYQPOkgWbkFPELuPtAo2DwAxgA24VZRjAZxuvV1TSnpPBo02EEkpfmFlV7VUOhYeFERqBg4BIEhYYtQAD4zPc2LwlIA9EdyAHoA-EA
+type CellTypeMapper<CT extends { type: string }> = {
+    [ct in CT as ct["type"]]: typeof ct
 }
 
-export interface ScientificNotationCell extends FloatCell {}
-
-export interface TextCell extends DefaultCell {
-    value: RawValueType
-    truncateTo?: number
-}
-
-export interface AnnotatedTextCell extends TextCell {
-    color?: Color
-    tooltip?: ReactNode | string
-}
-
-export interface BadgeCell extends AnnotatedTextCell {
-    backgroundColor?: Color
-    borderColor?: Color
-    icon?: BadgeIconType
-}
-
-export interface BooleanCell extends Omit<BadgeCell, 'value'> {
-    value: boolean
-    trueStr: string // what value to display for TRUE (e.g., TRUE, Yes, Y, Coding); FALSE inferred
-    nullAsFalse?: boolean // assume null === FALSE
-    falseStr?: string // if missing FALSE is displayed as empty string
-}
-
-export interface LinkCell extends AnnotatedTextCell {
-    url: string
-    newTab?: boolean
-}
-
-export interface PercentageBarCell extends Omit<DefaultCell, 'value'> {
-    value: number,
-    colors?: [Color, Color]
-}
-
-export type NumericCellTypes = DefaultCell | PercentageBarCell | FloatCell | ScientificNotationCell | BadgeCell
-export type StringCellTypes = DefaultCell | AnnotatedTextCell | TextCell | BadgeCell | BooleanCell | LinkCell
-export type CellTypes = NumericCellTypes | StringCellTypes
-
+export const CellType:CellTypeMapper = CellTypeMapper<NumericCells | StringCells>;
 
