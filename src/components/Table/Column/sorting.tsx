7e8ef0411d4e5c55b767a8c408f9d171fbc8ae6e
fossilfriend <egreenfest@gmail.com>
2024-05-03 14:26:39 -0400

    fixed temp import

--- src/components/Table/Column/sorting.tsx
+++ src/components/Table/Column/sorting.tsx
@@ -5,7 +5,7 @@
 
 import { Row, SortingFn } from "@tanstack/react-table";
 import { parseFieldValue } from "../utils";
-import { TableData } from "../deprecated/types";
+import { TableData } from "../deprecated/tables";
 
 const reSplitAlphaNumeric = /([0-9]+)/gm;
 
