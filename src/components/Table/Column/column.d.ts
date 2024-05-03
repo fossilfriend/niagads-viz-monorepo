b2d5f4ddcc47f4c6359a768fb478952b740ee5e1
fossilfriend <egreenfest@gmail.com>
2024-05-03 14:26:22 -0400

    removed accessor

--- src/components/Table/Column/column.d.ts
+++ src/components/Table/Column/column.d.ts
@@ -2,7 +2,4 @@
 import { Cell } from "./cell"
 import { isJSON } from "@text/utils";
 
-export const getCellValue = (value: Cell ) => { // all cells hava a "value" field
-    return value.value
-}
 
