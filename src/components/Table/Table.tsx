7677e230a96aa5f7301778627d1802035f077ff4
fossilfriend <egreenfest@gmail.com>
2024-05-23 12:38:41 -0400

    render table -- FIXME: cell rendering

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -1,23 +1,43 @@
 import React, { useMemo, useState, useEffect } from "react"
 import { withErrorBoundary } from "react-error-boundary";
-import { createColumnHelper, ColumnDef } from "@tanstack/react-table"
+import {
+    flexRender,
+    getCoreRowModel,
+    getPaginationRowModel,
+    useReactTable,
+    SortingState,
+    getSortedRowModel,
+    createColumnHelper,
+    ColumnDef,
+} from "@tanstack/react-table"
+
+import { ArrowDownIcon, ArrowUpIcon, ArrowsUpDownIcon } from "@heroicons/react/24/solid";
 
 import { _hasOwnProperty } from "@common/utils"
+import { errorFallback } from "@common/errors"
 
 import { SortConfig, GenericColumn, getColumn } from "./Column"
 import { Cell, GenericCell, getCellValue, renderCell, resolveCell, validateCellType } from "./Cell"
-import { TableOptions} from "./TableProperties"
-import { errorFallback } from "@common/errors"
+import { TableConfig } from "./TableProperties"
+import PaginationControls from "./PaginationControls";
 
+
+const TAILWIND_TABLE = {
+    table: "table-auto text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400 border-collapse",
+    thead: "uppercase text-xs text-left text-gray-700 bg-gray-50 dark:bg-gray-700 dark:text-gray-400",
+    th: "px-2",
+    htr: "",
+    td: "",
+    dtr: "hover:bg-gray-50 bg-white border-b dark:bg-gray-800 odd:border-gray-700"
+}
 
 export type TableRow = Record<string, GenericCell | GenericCell[]>
 export type TableData = TableRow[]
 export interface Table {
-    options?: TableOptions
+    options?: TableConfig
     columns: GenericColumn[]
     data: TableData
 }
-
 
 
 // FIXME: type of return should be custom sorting function
@@ -26,7 +46,7 @@ const __resolveSortingFn = (options: SortConfig) => {
     return _hasOwnProperty('sortingFn', options) ? options.sortingFn : 'alphanumeric'
 }
 
-const __resolveCell = (userCell: GenericCell | GenericCell[], column: GenericColumn, index:number) => {
+const __resolveCell = (userCell: GenericCell | GenericCell[], column: GenericColumn, index: number) => {
     try {
         const cell = resolveCell(userCell, column?.type)
         return cell
@@ -38,7 +58,7 @@ const __resolveCell = (userCell: GenericCell | GenericCell[], column: GenericCol
 
 const Table: React.FC<Table> = ({ columns, data, options }) => {
 
-    const [error, setError] = useState(null);
+    const [sorting, setSorting] = useState<SortingState>([]);
 
     const tableOptions: any = useMemo(() => {
         // from column definitions
@@ -49,7 +69,7 @@ const Table: React.FC<Table> = ({ columns, data, options }) => {
 
 
     // translate GenericColumns to ColumnDefs 
-    const resolvedColumns = useMemo(() => {
+    const resolvedColumns = useMemo<ColumnDef<TableRow>[]>(() => {
         const columnHelper = createColumnHelper<TableRow>();
         const columnDefs: ColumnDef<TableRow>[] = [];
         // TODO: add display column w/checkboxes if need row selection 
@@ -64,12 +84,11 @@ const Table: React.FC<Table> = ({ columns, data, options }) => {
             }
             columnDefs.push(
                 //columnHelper.accessor()
-                columnHelper.accessor(row => getCellValue(row[col.id] as Cell),
+                columnHelper.accessor(row => getCellValue(row[col.id as keyof typeof row] as Cell),
                     {
                         id: col.id,
                         // header: renderCellHeader(col.header, col.info),
-                        // TODO: renderCell does not need col.type b/c type will be in the value
-                        cell: props => renderCell(props.getValue() as Cell, col.type!),
+                        cell: props => renderCell(props.getValue() as Cell),
                         // sortingFn: col.sort !== undefined && __resolveSortingFn(col.sort)
                     }
                 )
@@ -78,11 +97,11 @@ const Table: React.FC<Table> = ({ columns, data, options }) => {
         return columnDefs;
     }, []);
 
-    const resolvedData = useMemo(() => {
-        const tableData: TableData = []
+    const resolvedData = useMemo<TableData>(() => {
+        let tableData: TableData = []
         const enFields = columns.length // expected number of fields
-        try {  
-            data.forEach((row: TableRow, index:number) => {
+        try {
+            data.forEach((row: TableRow, index: number) => {
                 // validate expected number of fields per row observed
                 const onFields = Object.keys(row).length // observed number of fields
                 if (onFields > enFields) {
@@ -103,20 +122,84 @@ const Table: React.FC<Table> = ({ columns, data, options }) => {
                     tableRow[columnId] = __resolveCell(value, currentColumn, index)
                 }
 
-                tableData.push(tableRow)
+                tableData.push(tableRow)   
             });
         }
         catch (e: any) {
             throw Error(e.message)
         }
+
         return tableData;
     }, [columns])
 
 
-    return <div>
-        <h3>Columns: {JSON.stringify(resolvedColumns)}</h3>
-        <p> Data: {JSON.stringify(resolvedData)}</p>
-    </div>
+    const table = useReactTable({
+        data: resolvedData, 
+        columns: resolvedColumns,
+        getCoreRowModel: getCoreRowModel(),
+        /*state: { sorting },
+        onSortingChange: setSorting,
+        getSortedRowModel: getSortedRowModel(),
+        getPaginationRowModel: getPaginationRowModel(),
+        defaultColumn: {
+            size: 150,
+            minSize: 20,
+            maxSize: 300,
+        },*/
+        // sortingFns: CustomSortingFunctions,
+    });
+
+
+    return ( 
+        table ? (<>
+            <table className={TAILWIND_TABLE.table}>
+                <thead className={TAILWIND_TABLE.thead}>
+                    {table.getHeaderGroups().map((headerGroup) => (
+                        <tr key={headerGroup.id} className={TAILWIND_TABLE.htr}>
+                            {headerGroup.headers.map((header) => {
+                                return (
+                                    <th key={header.id} colSpan={header.colSpan} scope="col" className={TAILWIND_TABLE.th}>
+                                        {header.isPlaceholder ? null : (
+                                            <div
+                                                style={
+                                                    header.column.getCanSort() ? { "cursor": "pointer" } : {}
+                                                }
+                                                onClick={header.column.getToggleSortingHandler()}
+                                            >
+                                                {flexRender(
+                                                    header.column.columnDef.header,
+                                                    header.getContext()
+                                                )}
+                                                {{
+                                                    sort: <ArrowsUpDownIcon className="h4 text-blue-600 pl-px" />,
+                                                    asc: <ArrowUpIcon className="h-4 text-blue-600 pl-px" />,
+                                                    desc: <ArrowDownIcon className="h-4 text-blue-600 pl-px" />,
+                                                }[header.column.getIsSorted() as string] ?? null}
+                                            </div>
+                                        )}
+                                    </th>
+                                );
+                            })}
+                        </tr>
+                    ))}
+                </thead>
+                <tbody>
+                    {table.getRowModel().rows.map((row) => (
+                        <tr key={row.id} className={TAILWIND_TABLE.dtr}>
+                            {row.getVisibleCells().map((cell) => (
+                                <td key={cell.id}>
+                                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                                </td>
+                            ))}
+                        </tr>
+                    ))}
+                </tbody>
+            </table >
+            <PaginationControls table={table} />
+        </>
+    ) : 
+    <div>No data</div>
+    )
 }
 
 
