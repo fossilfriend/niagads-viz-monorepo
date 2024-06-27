440db4562a958c6e021171c1f131582bda22b361
fossilfriend <egreenfest@gmail.com>
2024-06-27 11:41:25 -0400

    add info icons

--- src/components/Text/Text.tsx
+++ src/components/Text/Text.tsx
@@ -1,5 +1,9 @@
 import React from "react"
 
+import {
+    InformationCircleIcon
+} from "@heroicons/react/24/outline";
+
 import { _get, _hasOwnProperty } from "@common/utils";
 import { TAILWINDCSS_CLASSES } from "@common/tailwind"
 import { renderTooltip } from "@components/UI/Tooltip";
@@ -15,17 +19,25 @@ const _renderTextElement = (value: any, style: any, className: string) => {
 export const Text = <T,>({ props }: TextRenderer<T>) => {
     let style = {}
     const hasTooltip = _hasOwnProperty('tooltip', props)
+    const useInfoLink = _hasOwnProperty('inlineTooltip', props) // tooltip rendered as info link instead of info icon
 
     if (_hasOwnProperty('color', props)) {
         style = Object.assign({ 'color': _get('color', props) }, style)
     }
 
-    let textElement = _renderTextElement(_get('value', props), 
-        style,  
-        hasTooltip ? TAILWINDCSS_CLASSES['info_link'] : "")
+    let textElement = _renderTextElement(_get('value', props), style,
+        hasTooltip && useInfoLink ? TAILWINDCSS_CLASSES['info_link'] : "")
 
     if (hasTooltip) {
-        textElement = renderTooltip(textElement, _get('tooltip', props))
+        if (useInfoLink) {
+            textElement = renderTooltip(textElement, _get('tooltip', props))
+        }
+        else { // draw info icon and attach the tooltip to the icon
+            textElement = <div className="flex">
+                {textElement}
+                <InformationCircleIcon className="ml-1 size-3 text-blue-600" title={_get('tooltip', props)} />
+                </div>
+        }
     }
 
     return (<>{textElement}</>)
