aa760f193b7e4c3061d8c9f66b327e4a3e67e2c2
fossilfriend <egreenfest@gmail.com>
2024-06-27 10:08:46 -0400

    tooltip formatting

--- src/common/tailwind.tsx
+++ src/common/tailwind.tsx
@@ -4,3 +4,7 @@ import tailwindConfig from '../../tailwind.config.js'
 const TAILWIND_CONFIG = resolveConfig(tailwindConfig)
 export const TAILWIND_THEME = TAILWIND_CONFIG.theme
 const TAILWIND_COLORS = Object.keys(TAILWIND_THEME.colors)
+
+export const TAILWINDCSS_CLASSES = {
+    infolink: "underline decoration-dashed decoration-blue-500 underline-offset-4"
+}
--- src/components/Text/Text.tsx
+++ src/components/Text/Text.tsx
@@ -1,33 +1,31 @@
 import React from "react"
 
 import { _get, _hasOwnProperty } from "@common/utils";
-import { BasicType } from "@common/types";
-
-import { Tooltip, renderTooltip } from "@components/UI/Tooltip";
+import { TAILWINDCSS_CLASSES } from "@common/tailwind"
+import { renderTooltip } from "@components/UI/Tooltip";
 
 import { TextRenderer } from "./TextRendererProperties";
 
 
-const renderTextElement = (value: any, style: any) => {
-    return <span style={style}>{value}</span>
+const renderTextElement = (value: any, style: any, className: string) => {
+    return <span className={className} style={style}>{value}</span>
 }
 
 //const MyComponent = <T,>({ data }: MyComponentProps<T>) => {
 export const Text = <T,>({ props }: TextRenderer<T>) => {
     let style = {}
-    let tooltip = undefined
+    const hasTooltip = _hasOwnProperty('tooltip', props)
 
 
     if (_hasOwnProperty('color', props)) {
         style = Object.assign({ 'color': _get('color', props) }, style)
     }
 
-    const textElement = renderTextElement(_get('value', props), style)
+    const textElement = renderTextElement(_get('value', props), 
+        style,  
+        hasTooltip ?TAILWINDCSS_CLASSES['infolink'] : "")
 
-    if (_hasOwnProperty('tooltip', props)) {
-        //const key = `${_get('columnId', props)}_${_get('rowId', props)}`
-        tooltip = renderTooltip(textElement, _get('tooltip', props))
-    }
+    const tooltip = hasTooltip ? renderTooltip(textElement, _get('tooltip', props)) : undefined
 
     return (<>{tooltip ? tooltip : textElement}</>)
 
