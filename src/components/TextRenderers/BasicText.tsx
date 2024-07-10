79fb2628a683d84f570b5a47a31cfec70de248d9
fossilfriend <egreenfest@gmail.com>
2024-07-10 10:50:56 -0400

    broken - clob

--- src/components/TextRenderers/BasicText.tsx
+++ src/components/TextRenderers/BasicText.tsx
@@ -5,19 +5,20 @@ import { TAILWINDCSS_CLASSES } from "@common/tailwind"
 
 import { TextRenderer, renderWithInfo, renderStyledText, renderNullValue } from "./TextRenderer";
 
+
 // TODO: truncateTo --> long text
 
 
 export const Text = <T,>({ props }: TextRenderer<T>) => {
     let style = {}
     const hasTooltip = _hasOwnProperty('tooltip', props)
-    const useInfoLink = _hasOwnProperty('inlineTooltip', props) // tooltip rendered as info link instead of info icon
+    const useInfoLink = _get('inlineTooltip', props, false) // tooltip rendered as info link instead of info icon
+    const value = _get('value', props)
+    const maxLength = _get('truncateTo', props, 100)
 
     if (_hasOwnProperty('color', props)) {
         style = Object.assign({ 'color': _get('color', props) }, style)
     }
-
-    const value = _get('value', props)
 
     if (_isNull(value)) {
         return renderNullValue(_get('naString', props))
