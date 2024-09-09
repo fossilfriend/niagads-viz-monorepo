8fe0884decdc1d0235871451c649d5e7413047f7
fossilfriend <egreenfest@gmail.com>
2024-09-09 12:21:13 -0400

    pagination controls; layout done; behavior not fully tested

--- src/components/Table/PaginationControls.tsx
+++ src/components/Table/PaginationControls.tsx
@@ -1,25 +1,57 @@
-import React from "react"
+import React, { useState, useMemo } from "react"
+
 import { Table } from "@tanstack/react-table"
 
+import _range from "lodash.range"
 import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/24/solid'
 import { Select, Button } from "@components/UI"
+import range from "lodash.range"
 
-const __TAILWIND_CSS = {
-    stroke: 'stroke-black stroke-2'
-}
+
 interface PaginationControlsProps {
     table: Table<any>
 }
 
 // Results per page <select> start-end row count of total rows <previous> <next>
 
+const __generatePageSizeOptions = (nRows: number) => {
+    let nearestTenth = Math.ceil(nRows / 10) * 10
+
+    if (nearestTenth >= 500)
+        return [10, 20, 30, 40, 50, 100, 500]
+    else if (nearestTenth >= 100)
+        return [10, 20, 30, 40, 50, 100]
+    else if (nearestTenth >= 50)
+        return range(10, 60, 10) // range is up to but not including end
+    else if (nearestTenth > 10)
+        return range(10, nearestTenth + 10, 10)
+
+    return [10]
+}
+
 export const PaginationControls = ({ table }: PaginationControlsProps) => {
+    const [pageSize, setPageSize] = useState<number>(table.getState().pagination.pageSize)
+    const [nRows, setNRows] = useState<number>(table.getRowCount())
+    const pageSizeOptions = useMemo(() => (__generatePageSizeOptions(nRows)), [nRows])
+
+    const minDisplayedRow = table.getState().pagination.pageIndex * pageSize + 1
+    let maxDisplayedRow = minDisplayedRow + pageSize - 1
+    if (maxDisplayedRow > nRows) maxDisplayedRow = nRows
+
     return <>
         <div className="m-2">
-            <div className="flex float-right items-center gap-2">
-                <Select fields={[0, 10, 20, 50, 100, 500]} label="Results per page" id="pages" inline />
+            <div className="inline-flex float-right gap-2">
+                <Select defaultValue={pageSize.toString()} fields={pageSizeOptions} 
+                    onChange={(e:any) => {table.setPageSize(Number(e.target.value))}}
+                    label="Results per page" id="pages" inline mode='plain' />
+                <p className="text-sm text-gray-900 px-2">{minDisplayedRow} - {maxDisplayedRow} of {nRows}</p>
+                <Button mode="white" onClick={()=>table.previousPage()} disabled={!table.getCanPreviousPage()}>
+                    <ChevronLeftIcon className={`icon-button ${!table.getCanPreviousPage() ? 'icon-disabled' : ''}`}></ChevronLeftIcon>
+                </Button>
+                <Button mode="white" onClick={()=>table.nextPage()} disabled={!table.getCanNextPage()}>
+                    <ChevronRightIcon className={`icon-button ${!table.getCanNextPage() ? 'icon-disabled' : ''}`}></ChevronRightIcon>
+                </Button>
             </div>
-            <Button mode="white"><ChevronRightIcon className={`button-icon ${__TAILWIND_CSS.stroke}`}></ChevronRightIcon></Button>
         </div>
     </>
 }
@@ -34,19 +66,7 @@ const PaginationControls: React.FC<PaginationControlsProps> = ({
             <div className="h-2" />
             <div className="flex items-center gap-2">
               
-                <button
-                    className="border rounded p-1"
-                    onClick={() => table.previousPage()}
-                    disabled={!table.getCanPreviousPage()}
-                >
-                    {'<'}
-                </button>
-                <button
-                    className="border rounded p-1"
-                    onClick={() => table.nextPage()}
-                    disabled={!table.getCanNextPage()}
-                >
-                    {'>'}
+           
                 </button>
                 <button
                     className="border rounded p-1"
