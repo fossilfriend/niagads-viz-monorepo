75ff91d77c34e338d89060894f070f8d750c7ad1
fossilfriend <egreenfest@gmail.com>
2024-09-17 11:05:51 -0400

    rowSelect options

--- src/components/Table/TableProperties.tsx
+++ src/components/Table/TableProperties.tsx
@@ -2,9 +2,10 @@ import { BasicType, FileFormat } from "@common/types"
 import { GenericCell } from "@table/Cell"
 
 export interface RowSelectOptions {
-    onSelectFn: any // behavior on select
-    selectType: 'highlight' | 'checkbox' // how row selection is indicated
-    multiSelect?: boolean // optional: allow selection of multiple rows, false if missing
+    onRowSelect: any // behavior on select
+    header: string,
+    description?: string,
+    disableMultiSelect?: boolean // optional: allow selection of multiple rows, false if missing
 }
 
 interface SortConfig {[column: string]: 'asc' | 'desc'}
@@ -16,6 +17,7 @@ interface InitialTableState {
     sort?: SortConfig
     filter?: FilterConfig[]
 }
+
 export interface TableConfig {
     title?: string
     id?:string // optional: internal id for the table
@@ -23,10 +25,11 @@ export interface TableConfig {
     description?: string // optional: descriptive text describing the table for info popup
     canFilter?: boolean // optional: disables all filtering on the table when false; default TRUE
     export?: FileFormat[] // optional: enable exports in the listed file formats; exports disabled if missing
-    rowSelect?: RowSelectOptions // optional: enables row selection and related state change options
     columOrder?: string[] // optional: specify columns by id in display order; else will use order in table definition
     defaultColumns?: string[] // optional: any column ids not listed will be hidden by default
     onTableLoad?: any,
+    rowSelect?: RowSelectOptions
+    disableMultiSelect?: boolean //optional: only allows a single row to be selected; uses radio buttons instead of checkboxes
 }
 
 export type TableRow = Record<string, GenericCell | GenericCell[]>
