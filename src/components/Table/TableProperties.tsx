29a7a775539dd31d35d1b9817ed50eb34c2c75ba
fossilfriend <egreenfest@gmail.com>
2024-09-19 15:40:20 -0400

    rowSelect options: specify value returned when row is selected, a list of preselected rows

--- src/components/Table/TableProperties.tsx
+++ src/components/Table/TableProperties.tsx
@@ -6,6 +6,8 @@ export interface RowSelectOptions {
     header: string,
     description?: string,
     enableMultiRowSelect?: boolean // optional: allow selection of multiple rows, false if missing
+    selectedValues?: string[], // a list of unique row identifiers the be selected in advance
+    rowId?: string // specify a field containing unique values to return as the row_id (e.g., browser track key) instead of the index
 }
 
 interface SortConfig {[column: string]: 'asc' | 'desc'}
