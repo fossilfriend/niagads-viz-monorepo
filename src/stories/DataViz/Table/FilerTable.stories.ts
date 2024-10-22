86ba630c29c5b7f9574332a0c1901fbd04daad55
fossilfriend <egreenfest@gmail.com>
2024-10-22 11:17:56 -0400

    FILER table

---
+++ src/stories/DataViz/Table/FilerTable.stories.ts
@@ -0,0 +1,28 @@
+//@ts-nocheck
+import type { Meta, StoryObj } from '@storybook/react';
+
+import Table from '@table/Table';
+import { TABLE_DEFINTION as table } from '../../../../example/data/table_filer.tsx';
+
+const meta: Meta<typeof Table> = {
+    title: 'NIAGADS-VIZ/Data Vizualization/Table/FILER Table',
+    component: Table,
+    parameters: {
+        // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
+        layout: 'fullscreen',
+    },
+    // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
+    tags: ['autodocs'],
+};
+
+export default meta;
+type Story = StoryObj<typeof Table>;
+
+export const Default: Story = {
+    args: {
+        id: table.id,
+        columns: table.columns,
+        options: table.options,
+        data: table.data
+    }
+};
