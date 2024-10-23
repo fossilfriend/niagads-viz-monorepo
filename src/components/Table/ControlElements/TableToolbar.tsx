715382a946d1d61e2e8f3290312992bff0908516
bug-sam <samuel.tate@outlook.com>
2024-10-23 12:17:41 -0400

    format table.tsx; add column controls deopdown

--- src/components/Table/ControlElements/TableToolbar.tsx
+++ src/components/Table/ControlElements/TableToolbar.tsx
@@ -1,12 +1,12 @@
 import React, { useState, useMemo } from "react"
 
 import { Column as ReactTableColumn, Table as ReactTable } from "@tanstack/react-table"
-import { ViewColumnsIcon, ArrowDownTrayIcon } from '@heroicons/react/24/solid'
 
 import { Button, SearchInput, Tooltip } from "@components/UI"
 import { FileFormat } from "@common/types"
 import { TableRow } from "@table/TableProperties"
 import { TableExportControls } from "./TableExportControls"
+import { ColumnControls } from "./ColumnControls"
 
 
 // column.getCanHide
@@ -31,10 +31,7 @@ export const TableToolbar = ({ table, exportTypes }: ToolbarProps) => {
     return <div className="relative flex justify-end gap-2 m-2">
         <SearchInput value={table.getState().globalFilter} onChange={val => table.setGlobalFilter(val)} />
         {canToggleColumns && <Tooltip message="Show/Hide Columns">
-            <Button variant="white" onClick={() => alert('show hide columns')} >
-                <ViewColumnsIcon className={`icon-button`}></ViewColumnsIcon>
-                <span className="ml-2 uppercase">Columns</span>
-            </Button>
+            <ColumnControls columns={table.getAllLeafColumns()} onSelect={() => console.log("selected")} />
         </Tooltip>}
         {exportTypes && <Tooltip message="export table data">
             <TableExportControls isFiltered={tableIsFiltered} exportOptions={exportTypes}/>
