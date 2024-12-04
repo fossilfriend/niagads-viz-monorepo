80383d0211546a0970dc703ecde801378afd8bcb
fossilfriend <egreenfest@gmail.com>
2024-12-04 10:54:35 -0500

    json syntax highlighting

--- common/utils.tsx
+++ common/utils.tsx
@@ -70,3 +70,24 @@ export const toExponential = (value: string | number, precision: number = 2) => 
 export const toTitleCase = (value: string) => (
     value.replace(/\w\S*/g,text => text.charAt(0).toUpperCase() + text.substring(1).toLowerCase())
 )
+
+// adapted from https://jsfiddle.net/KJQ9K/554/
+export function jsonSyntaxHighlight(json: any) {
+    json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
+    return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, 
+        function (match: string) {
+        var cls = 'number';
+        if (/^"/.test(match)) {
+            if (/:$/.test(match)) {
+                cls = 'key';
+            } else {
+                cls = 'string';
+            }
+        } else if (/true|false/.test(match)) {
+            cls = 'boolean';
+        } else if (/null/.test(match)) {
+            cls = 'null';
+        }
+        return '<span class="' + cls + '">' + match + '</span>';
+    });
+}
