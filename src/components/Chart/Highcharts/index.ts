49ca0d1b41df2993dc1f4ed8244574656ff63dd4
fossilfriend <egreenfest@gmail.com>
2024-12-04 09:47:47 -0500

    remove high charts

--- src/components/Chart/Highcharts/index.ts
+++
@@ -1,6 +0,0 @@
-import HighchartsPlot from "./HighchartsPlot";
-import { HighchartsTableTrellis, HighchartsColumnTrellis } from "./HighchartsTrellisPlot";
-
-export * from "./HighchartsOptions";
-
-export { HighchartsPlot, HighchartsTableTrellis, HighchartsColumnTrellis };
