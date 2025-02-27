09de8186bebf29851516f107f46be11cd7f25db6
fossilfriend <egreenfest@gmail.com>
2025-02-27 14:24:33 -0500

    fix path

--- packages/Table/tailwind.config.js
+++ packages/Table/tailwind.config.js
@@ -4,8 +4,8 @@ const {heroui} = require("@heroui/theme");
 module.exports = {
     content: [
         "./src/**/*.{js,jsx,ts,tsx}",
-        "./node_modules/@heroui/theme/dist/components/(button|dropdown|slider).js",
-        "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}",
+        "../../node_modules/@heroui/theme/dist/components/(button|dropdown|slider).js",
+        "../../node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}",
     ],
     purge: ['./src/**/*.{js,jsx,ts,tsx}'],
     plugins: [heroui()],
