2805508124a9694fa37d4b8ec6070cccbee25043
fossilfriend <egreenfest@gmail.com>
2025-02-27 10:12:30 -0500

    fix purge

--- packages/Table/tailwind.config.js
+++ packages/Table/tailwind.config.js
@@ -3,9 +3,7 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: ["./src/**/*.{js,jsx,ts,tsx}"],
-    purge: {
-        content: ['./src/**/*.{js,jsx,ts,tsx}']
-    },
+    purge: ['./src/**/*.{js,jsx,ts,tsx}'],
     plugins: [],
 };
 
