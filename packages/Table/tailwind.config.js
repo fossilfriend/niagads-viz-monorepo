97c1433d947f1fbac00c92e27db87b7c381a055e
fossilfriend <egreenfest@gmail.com>
2025-02-27 13:27:23 -0500

    add heroui

--- packages/Table/tailwind.config.js
+++ packages/Table/tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
+const {heroui} = require("@heroui/theme");
+
 module.exports = {
-    content: ["./src/**/*.{js,jsx,ts,tsx}"],
+    content: [
+        "./src/**/*.{js,jsx,ts,tsx}",
+        "./node_modules/@heroui/theme/dist/components/(button|dropdown|slider).js",
+        "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}",
+    ],
     purge: ['./src/**/*.{js,jsx,ts,tsx}'],
-    plugins: [],
+    plugins: [heroui()],
 };
 
 
