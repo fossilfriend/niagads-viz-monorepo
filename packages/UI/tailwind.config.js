bcf44dd13cf347fd30741ad07855259ebfc926fd
fossilfriend <egreenfest@gmail.com>
2025-02-27 13:30:34 -0500

    clean config/ tailwind 4

--- packages/UI/tailwind.config.js
+++ packages/UI/tailwind.config.js
@@ -1,24 +1,12 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
-
+const {heroui} = require("@heroui/theme");
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
-    content: ["./src/**/*.{js,jsx,ts,tsx}"],
-    theme: {
-        extend: {
-            colors: {
-                'primary': '#3d5263',
-                'secondary': '#f9c664',
-                'accent': '#618eb5',
-                'accent-dark': '#27333f'
-            },
-            fontFamily: {
-                'sans': ['"Open Sans"', ...defaultTheme.fontFamily.sans],
-                'inter': ['Inter'],
-                'lato': ['Lato']
-            }
-        },
-    },
-    plugins: [],
+    content: [
+        "./src/**/*.{js,jsx,ts,tsx}",
+    ],
+    purge: ['./src/**/*.{js,jsx,ts,tsx}'],
+    plugins: [heroui()],
 };
 
 
