720b2cafc02c32e486559c6e353fbd798b33fe5e
fossilfriend <egreenfest@gmail.com>
2025-03-11 12:33:14 -0400

    change extension

--- packages/Table/tailwind.config.mjs
+++ packages/Table/tailwind.config.mjs
@@ -4,7 +4,6 @@ const {heroui} = require("@heroui/theme");
 module.exports = {
     content: [
         "./src/**/*.{js,jsx,ts,tsx}",
-        "../../node_modules/@heroui/theme/dist/components/(button|dropdown|slider).js",
         "../../node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}",
     ],
     purge: ['./src/**/*.{js,jsx,ts,tsx}'],
